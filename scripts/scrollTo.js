4a57ddf62994e0be94743f354a6d1db85d5d9a5a
Bloodlister <AsenJ.Mihaylov@gmail.com>
2017-01-20 19:24:32 +0200

    Added files

---
+++ scripts/scrollTo.js
@@ -0,0 +1,19 @@
+$(document).ready(function (){
+			$("#toAbout").click(function (){
+				$('html, body').animate({
+					scrollTop: $("#home").offset().top
+				}, 1000);
+			});
+			
+			$("#toProjects").click(function (){
+                $('html, body').animate({
+                    scrollTop: $("#projects").offset().top
+                }, 1000);
+            });
+			
+			$("#toContact").click(function (){
+                $('html, body').animate({
+                    scrollTop: $("#contact").offset().top
+                }, 1000);
+            });
+        });
